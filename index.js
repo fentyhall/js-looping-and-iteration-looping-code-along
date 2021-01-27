d70804126515de1330915f152c521b8aa0dc1732
fentyhall <fentymhall@gmail.com>
2021-01-27 14:11:46 -0800

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function writeCards(arr, event) {
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+        arr[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
     }
     return arr;
 }
