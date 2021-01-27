d79c81e2a6662faf4c8c9ce4286ddcc418dceaae
fentyhall <fentymhall@gmail.com>
2021-01-27 14:09:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+    }
+    return arr;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
